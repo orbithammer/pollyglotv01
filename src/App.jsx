e45c486332b77a12938ed0cedda6bcbc0822dae8
Brock Penner <orbithammer@users.noreply.github.com>
2024-01-02 10:39:57 +0900

    finishing up css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import OpenAI from "openai"
 import imageUrl from "/src/assets/parrot.png"
 
-console.log("10:33")
+console.log("10:39")
 function App() {
     const [isTranslating, setIsTranslating] = React.useState(false)
     const [phrase, setPhrase] = React.useState("")
